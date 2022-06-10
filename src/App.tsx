2fedaabbc435d04140b6b64d6dbc35752e9d7827
박지윤 <jyun1937@naver.com>
2022-06-10 18:56:10 +0900

    no use file remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Router from "./Router";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
